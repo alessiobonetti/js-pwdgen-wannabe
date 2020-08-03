82233a0b3a8b9710b4f2c280c6a3670abe8d040b
Alessio Bonetti <alesine87@gmail.com>
2020-08-03 15:47:43 +0200

    Js Nome cognome colore

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// inserimento dati
+
+// nome
+var nome = prompt("Inserisci il tuo nome");
+
+// cognome
+var cognome = prompt("Inserisci il tuo cognome");
+
+// colore
+
+var colore = prompt("Inserisci il tuo colore preferito");
+
+// Stampa risultato
+
+document.getElementById("risultato").innerHTML = nome + cognome + colore + "19";
